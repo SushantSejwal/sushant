8d171471854d94546c644b1c7c5af1d4d6887b37
Sushant Sejwal <87465442+SushantSejwal@users.noreply.github.com>
2021-10-31 16:47:06 +0530

    Add files via upload

--- Python/js/komal.js
+++ Python/js/komal.js
@@ -62,24 +62,35 @@ const bullet_svg_g = document.querySelector('.bullet-svg-g');
 
 const media_phone = window.matchMedia('(max-width: 768px)')
 if (media_phone.matches) {
-
-    bullet.addEventListener("click", () => {
-
-        bullet_svg_g.classList.toggle("sushant_bullet_phone");
-        bullet_audio_toogle()
-
-    });
-
     function bullet_audio_toogle() {
 
         if (bullet_audio.paused) {
             bullet_audio.play();
         }
-        
+
         else {
             bullet_audio.pause();
         }
     };
+
+    function bullet_svg_grey() {
+        if (bullet_audio.paused) {
+            bullet_svg_g.classList.remove("sushant_bullet_phone");
+        }
+    }
+
+    function bullet_svg_white() {
+        if (bullet_audio.played) {
+            bullet_svg_g.classList.add("sushant_bullet_phone");
+        }
+    }
+    
+    bullet.addEventListener("click", () => {
+        bullet_svg_white();
+        bullet_audio_toogle();
+        bullet_svg_grey();
+
+    });
 }
 
 else {
